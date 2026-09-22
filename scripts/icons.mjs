332c2a2d38285d9175c2c618857514f63a73c5e4
kimwonho-poet <qhrnsj1234@gmail.com>
2026-09-22 11:20:17 +0900

    Build reading assets and footnote toolbar icon

--- scripts/icons.mjs
+++ scripts/icons.mjs
@@ -1,6 +1,7 @@
 import { readFile, writeFile, mkdir } from 'node:fs/promises';
 const names = ['external-link', 'log-out', 'log-in', 'plus', 'save', 'trash-2', 'arrow-up', 'arrow-down', 'refresh-cw', 'eye', 'pencil', 'download', 'check', 'x', 'bold', 'italic', 'underline', 'strikethrough', 'quote', 'list', 'list-ordered', 'link', 'image', 'minus', 'undo-2', 'redo-2', 'align-left', 'align-center', 'align-right', 'panel-left', 'maximize-2', 'minimize-2', 'settings-2', 'arrow-left', 'more-horizontal', 'unlink', 'file-text', 'cloud-check'];
 const symbols = [];
+names.push('superscript');
 for (const name of names) {
   const svg = await readFile(`node_modules/lucide-static/icons/${({ 'more-horizontal': 'ellipsis', 'cloud-check': 'cloud' })[name] || name}.svg`, 'utf8');
   const body = svg.match(/<svg\b[^>]*>([\s\S]*?)<\/svg>/)?.[1];
